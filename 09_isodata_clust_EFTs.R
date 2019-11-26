865e3976fae3dbf3d9a83017735503364a1d237a
Xavier Rotllan-Puig <xavi.rotllan.puig@gmail.com>
2019-11-26 11:46:51 +0100

    bugs in ISODATA

--- 09_isodata_clust_EFTs.R
+++ 09_isodata_clust_EFTs.R
@@ -8,33 +8,51 @@ source("E:\\rotllxa\\LPD\\LPD/00_settings.R")
 ## Reading in data from 'final PCA' (Step 08) ####
 
 load(file = paste0(path2tempResults, "/results_Step8.RData"), verbose = TRUE)
-pca_final_raster
-summary(pca_final)
-
-head(as.data.frame(pca_final_raster))
-nrow(as.data.frame(pca_final_raster))
-as.data.table(as.data.frame(pca_final_raster))
-
-apply(as.data.table(as.data.frame(pca_final_raster)), 2, sd, na.rm = TRUE)
-apply(as.data.table(as.data.frame(pca_final_raster)), 2, mean, na.rm = TRUE)
-
+#pca_final_raster
+#summary(pca_final)
+#
+#head(as.data.frame(pca_final_raster))
+#nrow(as.data.frame(pca_final_raster))
+#as.data.table(as.data.frame(pca_final_raster))
+#
+#apply(as.data.table(as.data.frame(pca_final_raster)), 2, sd, na.rm = TRUE)
+#apply(as.data.table(as.data.frame(pca_final_raster)), 2, mean, na.rm = TRUE)
+#
 
 #initial data set to get clustered
 pca_data_ini <- as.data.frame(pca_final_raster)
-head(pca_data_ini)
-apply(pca_data_ini, 2, function(x) sum(is.na(x)))
-sum(!complete.cases(pca_data_ini))
+#head(pca_data_ini)
+#tail(pca_data_ini)
+#range(as.numeric(rownames(pca_data_ini)))
+#apply(pca_data_ini, 2, function(x) sum(is.na(x)))
+#sum(!complete.cases(pca_data_ini))
+
+sampling2test <- "Yes"
+sampling2test <- "No"
+if(sampling2test == "Yes"){
+  nrow(pca_data_ini)
+  toSample <- sample(rownames(pca_data_ini), (nrow(pca_data_ini) / 10))
+  head(toSample)
+  pca_data_ini_4later <- pca_data_ini[!rownames(pca_data_ini) %in% toSample, ]
+  pca_data_ini <- pca_data_ini[rownames(pca_data_ini) %in% toSample, ]
+}
+
 
 pca_data_ini_NA <- pca_data_ini[!complete.cases(pca_data_ini), ]   #to be used at the end to fill raster stack
 pca_data_ini <- pca_data_ini[complete.cases(pca_data_ini), ] 
-nrow(pca_data_ini)
-head(pca_data_ini)
+  
+
+
+
+#nrow(pca_data_ini)
+#head(pca_data_ini)
 
 
 ## Settings for clustering ####
 #number of initial clusters
-nclust_ini <- 15 # initial number of clusters (hard-coded; 10 < nclust_ini < 500)
 nclust_ini <- 50 # initial number of clusters (hard-coded; 10 < nclust_ini < 500)
+nclust_ini <- 15 # initial number of clusters (hard-coded; 10 < nclust_ini < 500)
+
 
 max_num_clustrs <- 500
 
@@ -154,33 +172,41 @@ repeat{
   
   ## Dist Inter-Clusters (centroids; if smaller than a threshold, merge clusters)
   dist_interclust <- dist(clust_centr_ini)
-  summary(dist_interclust)
-  quantile(dist_interclust, seq(0, 1, 0.05))
+  #summary(dist_interclust)
+  #quantile(dist_interclust, seq(0, 1, 0.05))
   
-  percentile <- ecdf(dist_interclust)
-  if((round(percentile(min_dist_interclust), 2) * 100) > 5) stop("Your minimum distance intercluster is bigger than 5th percentile.\nThis probably means a lot of cluster mergings. Are you happy with that???") 
+  #percentile <- ecdf(dist_interclust)
+  #if((round(percentile(min_dist_interclust), 2) * 100) > 5) stop("Your minimum distance intercluster is bigger than 5th percentile.\nThis probably means a lot of cluster mergings. Are you happy with that???") 
   
   small_centroids <- dist_interclust[dist_interclust < min_dist_interclust] 
   dist_interclust <- as.data.frame(as.matrix(dist_interclust))
   
   clust_mergd <- c()
   if(sum(small_centroids) > 0){
-    for (sml_ctrds in small_centroids){
+    for (sml_ctrds in sort(small_centroids)){ #sorting them we make sure that are taken into account the closest pair of clusters 
       clusts2merge <- rownames(which(dist_interclust == sml_ctrds, arr.ind = TRUE))
       #clusts2merge <- (which(dist_interclust == sml_ctrds, arr.ind = TRUE))
       #print(sml_ctrds)
       clust_mergd <- c(clust_mergd, clusts2merge)
-      centroids_back <- clust_centr_ini[rownames(clust_centr_ini) %in% clusts2merge, ]
-      centroids_back$closest <- 0
-      centroids_back$dist2closest <- 0
-      centroids_back
-      pca_data_ini_noCentr <- rbind(pca_data_ini_noCentr, centroids_back)
-      pca_data_ini_noCentr <- pca_data_ini_noCentr[order(as.numeric(rownames(pca_data_ini_noCentr))), ]
-      clust_centr_ini <- clust_centr_ini[!rownames(clust_centr_ini) %in% clusts2merge, ]
       
-      sml_cluster2merge <- pca_data_ini_noCentr[pca_data_ini_noCentr$closest %in% clusts2merge, ]
-      sml_cluster2merge <- sml_cluster2merge[sample(nrow(sml_cluster2merge), 1), ][, 1:nvars]
-      clust_centr_ini <- rbind(clust_centr_ini, sml_cluster2merge)
+#el problema es que quan n hi ha molts pot passar que un centroide s elimini de la taula en un cicle d abans i llavors en aquest punt no selecciona res
+#i dona l error. el merge s hauria de fer de tots els que estan a prop a la vegada
+
+      if(all(clusts2merge %in% rownames(clust_centr_ini))){
+        centroids_back <- clust_centr_ini[rownames(clust_centr_ini) %in% clusts2merge, ]
+        centroids_back$closest <- NA
+        centroids_back$dist2closest <- NA
+        centroids_back
+        pca_data_ini_noCentr <- rbind(pca_data_ini_noCentr, centroids_back)
+        pca_data_ini_noCentr <- pca_data_ini_noCentr[order(as.numeric(rownames(pca_data_ini_noCentr))), ]
+        clust_centr_ini <- clust_centr_ini[!rownames(clust_centr_ini) %in% clusts2merge, ]
+        
+        sml_cluster2merge <- pca_data_ini_noCentr[pca_data_ini_noCentr$closest %in% clusts2merge, ]
+        sml_cluster2merge <- sml_cluster2merge[sample(nrow(sml_cluster2merge), 1), ][, 1:nvars]
+        clust_centr_ini <- rbind(clust_centr_ini, sml_cluster2merge)
+        pca_data_ini_noCentr <- pca_data_ini_noCentr[!rownames(pca_data_ini_noCentr) %in% rownames(sml_cluster2merge), ]
+        
+      }
     }
   }
   clust_mergd
@@ -208,8 +234,8 @@ repeat{
   
   if(length(big_clusters) > 0){
     centroids_back <- clust_centr_ini[rownames(clust_centr_ini) %in% big_clusters, ]
-    centroids_back$closest <- 0
-    centroids_back$dist2closest <- 0
+    centroids_back$closest <- NA
+    centroids_back$dist2closest <- NA
     centroids_back
     pca_data_ini_noCentr <- rbind(pca_data_ini_noCentr, centroids_back)
     pca_data_ini_noCentr <- pca_data_ini_noCentr[order(as.numeric(rownames(pca_data_ini_noCentr))), ]
@@ -220,6 +246,7 @@ repeat{
       bg_cluster2split <- pca_data_ini_noCentr[pca_data_ini_noCentr$closest %in% bg_clstr, ]
       bg_cluster2split <- bg_cluster2split[sample(nrow(bg_cluster2split), 2), ][, 1:nvars]
       clust_centr_ini <- rbind(clust_centr_ini, bg_cluster2split)
+      pca_data_ini_noCentr <- pca_data_ini_noCentr[!rownames(pca_data_ini_noCentr) %in% rownames(bg_cluster2split), ]
     }
   }
   
@@ -248,20 +275,89 @@ repeat{
 
 
 
+
+
+
 # some checkings
-iters <- c("iter1", "iter2", "iter3")
+
+iters <- c("iter1", "iter2", "iter3") 
+iters <- paste0("iter", 1:iter_num) 
+#afegir totes les iteracions que s han generat (max 100) i comprovar els wilks
 
 for (i in iters) {
   print(i)
   load(paste0(path2tempResults, "/results_Step9_", i, ".RData"), verbose = FALSE)
   print(paste0("clust_mergd: ", paste0(clust_mergd, collapse = ", ")))
   print(paste0("big_clusters to split: ", paste0(big_clusters, collapse = ", ")))
-  assign(paste0("pca_data_ini_noCentr_", i), pca_data_ini_noCentr)
+  #assign(paste0("pca_data_ini_noCentr_", i), pca_data_ini_noCentr)
   frmla <- as.formula(paste0("closest ~ ", paste0(colnames(pca_data_ini_noCentr)[1:nvars], collapse = " + ")))
   wilks_formula <- Wilks.test(frmla, data = pca_data_ini_noCentr)
   print(paste0("Wilks' Lambda for... ", i))
-  print(wilks_formula)
+  print(wilks_formula$statistic)
 }
+
+for (i in iters) {
+  print(i)
+  load(paste0(path2tempResults, "/results_Step9_", i, ".RData"), verbose = FALSE)
+  #print(paste0("clust_mergd: ", paste0(clust_mergd, collapse = ", ")))
+  #print(paste0("big_clusters to split: ", paste0(big_clusters, collapse = ", ")))
+  #assign(paste0("pca_data_ini_noCentr_", i), pca_data_ini_noCentr)
+  frmla <- as.formula(paste0("closest ~ ", paste0(colnames(pca_data_ini_noCentr)[1:nvars], collapse = " + ")))
+  wilks_formula <- Wilks.test(frmla, data = pca_data_ini_noCentr)
+  print(paste0("Wilks' Lambda for... ", i))
+  print(wilks_formula$statistic)
+  print(wilks_formula$parameter)
+  print(wilks_formula$p.value)
+}
+
+for (i in iters) {
+  print(i)
+  load(paste0(path2tempResults, "/results_Step9_", i, ".RData"), verbose = FALSE)
+  print(paste0("clust to merge unique: ", length(unique(clust_mergd))))
+  print(paste0("nrow(clust_centr_ini): ", nrow(clust_centr_ini)))
+}
+
+for (i in iters) {
+  print(i)
+  load(paste0(path2tempResults, "/results_Step9_", i, ".RData"), verbose = FALSE)
+  print((dim(pca_final_raster)[1] * dim(pca_final_raster)[2]) == (nrow(pca_data_ini_4later) + nrow(pca_data_ini_NA) + nrow(pca_data_ini_noCentr[, 1:4]) + nrow(clust_centr_ini)))
+}
+
+
+
+
+jpeg(paste0(path2saveTests, "\\clusters.jpg"), width = 1500, height = 1500, res = 300)
+par(mfcol=c(3, 4))
+
+for (i in iters) { 
+  load(paste0(path2tempResults, "/results_Step9_", i, ".RData"), verbose = FALSE)
+
+  pca_final_raster1 <- pca_final_raster
+
+  nrow(pca_data_ini) == (nrow(pca_data_ini_noCentr[, 1:5]) + nrow(clust_centr_ini))
+  (dim(pca_final_raster)[1] * dim(pca_final_raster)[2]) == (nrow(pca_data_ini_4later) + nrow(pca_data_ini_NA) + nrow(pca_data_ini_noCentr[, 1:4]) + nrow(clust_centr_ini))
+  
+  
+  all_data <- rbind(clust_centr_ini, pca_data_ini_NA, pca_data_ini_4later)
+  all_data$closest <- NA
+  head(all_data)
+  
+  all_data <- rbind(pca_data_ini_noCentr[, 1:5], all_data)
+  all_data <- all_data[order(as.numeric(rownames(all_data))), ]
+  
+  pca_final_raster1 <- setValues(pca_final_raster1, as.matrix(as.numeric(all_data$closest)))
+  names(pca_final_raster1) <- "centroid"
+  pca_final_raster1
+  
+  pal <- colorRampPalette(c("red", "wheat2", "skyblue2", "blue", "yellow"))
+  par(xpd = FALSE)
+  plot(pca_final_raster1, col = pal(length(unique(all_data$closest))), legend = FALSE) 
+  
+  
+  
+}
+
+dev.off()
 
 
 
