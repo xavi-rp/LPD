71ad966779d34bdb2243bdc3eafb483f4009348a
Xavier Rotllan-Puig <xavi.rotllan.puig@gmail.com>
2019-12-16 15:57:30 +0100

    - Error ordering pixels after merging - Cleaning

--- 10_LocalNetProductivityScaling.R
+++ 10_LocalNetProductivityScaling.R
@@ -30,57 +30,43 @@ CycleFraction_rstr_average <- clusterR(CycleFraction_rstr, calc, args = list(fun
 endCluster()
 
 CycleFraction_rstr_average
-
+stuff2save <- c("CycleFraction_rstr_average")
+save(list = stuff2save, file = paste0(path2tempResults, "/results_Step10.RData"))
+#load(file = paste0(path2tempResults, "/results_Step10.RData"), verbose = TRUE)
 
 
 ## Merging Cyclic Fraction data with new clusters ####
 CycleFraction_average_df <- as.data.frame(CycleFraction_rstr_average)
-head(CycleFraction_average_df)
-nrow(CycleFraction_average_df)
-
-head(pca_data_ini_clusters[, "new_clst", drop=FALSE])
-nrow(pca_data_ini_clusters[, "new_clst", drop=FALSE])
-
 
 CycleFraction_average_df <- as.data.frame(cbind(CycleFraction_average_df, pca_data_ini_clusters[, "new_clst", drop=FALSE]))
 names(CycleFraction_average_df) <- c("CyclicFraction", "EFT")
-head(CycleFraction_average_df)
-nrow(CycleFraction_average_df)
+CycleFraction_average_df$rwnms <- as.numeric(rownames(CycleFraction_average_df))
 
 
 ## Calculating 90-percentile by EFT ####
 ## EFT = 0 is NoData in the raster
 CycleFraction_90perc <- as.data.frame(CycleFraction_average_df %>% group_by(EFT) %>% summarise_at(.vars = "CyclicFraction", .funs = c("CyclicFraction_90perc" = quantile), prob = 0.9, na.rm = TRUE))
-CycleFraction_90perc
-str(CycleFraction_90perc)
 
 
 ## Assigning maximum (potential) productivity to outliers ####
 ## outliers:  value > percentile 90 intra-cluster
 
 CycleFraction_average_df <- merge(CycleFraction_average_df, CycleFraction_90perc, by = "EFT", all.x = TRUE) 
-head(CycleFraction_average_df)
+CycleFraction_average_df <- CycleFraction_average_df[order(CycleFraction_average_df$rwnms), ]
 
 cond <- CycleFraction_average_df$CyclicFraction > CycleFraction_average_df$CyclicFraction_90perc & !is.na(CycleFraction_average_df$CyclicFraction)
 CycleFraction_average_df$CyclicFraction[cond] <- CycleFraction_average_df$CyclicFraction_90perc[cond]
+CycleFraction_average_df <- CycleFraction_average_df[, !names(CycleFraction_average_df) %in% "rwnms"]
 
-CycleFraction_average_df[rownames(CycleFraction_average_df) == 906635, ]
+CycleFraction_average_df$CyclicFraction_90perc[is.na(CycleFraction_average_df$CyclicFraction)] <- NA
 
 
 
 ## Calculating Local Scaled Productivity (LSP) ####
 ## Current status of efficiency of productivity: % over "potential", per pixel
 
-head(CycleFraction_average_df)
-
 CycleFraction_average_df$LSP <- round(((CycleFraction_average_df$CyclicFraction / CycleFraction_average_df$CyclicFraction_90perc) * 100), 1)
-head(CycleFraction_average_df)
-head(CycleFraction_average_df[CycleFraction_average_df$CyclicFraction < CycleFraction_average_df$CyclicFraction_90perc
-                                 & !is.na(CycleFraction_average_df$CyclicFraction), ])
-
 names(CycleFraction_average_df)[3] <- "PotentialProduction"
-head(CycleFraction_average_df)
-nrow(CycleFraction_average_df)
 
 
 
@@ -89,12 +75,12 @@ nrow(CycleFraction_average_df)
 
 LocalNetProductivity_rstr <- setValues(CycleFraction_rstr[[1:4]], as.matrix(CycleFraction_average_df))
 
-stuff2save <- c("CycleFraction_average_df", "LocalNetProductivity_rstr")
+stuff2save <- c(stuff2save, "CycleFraction_average_df", "LocalNetProductivity_rstr")
 save(list = stuff2save, file = paste0(path2tempResults, "/results_Step10.RData"))
-writeRaster(LocalNetProductivity_rstr, paste0(path2saveTests, "/LocalNetProductivity.tif"), overwrite = TRUE)
+writeRaster(LocalNetProductivity_rstr, paste0(path2saveTests, "/LocalNetProductivity.tif"), bylayer = TRUE, suffix = c(names(LocalNetProductivity_rstr)), overwrite = TRUE)
 
 
-
+#cellStats(LocalNetProductivity_rstr, max)
 
 
 
